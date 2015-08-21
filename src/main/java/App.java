773118e9df49f35dc4adf4e4f5c02489bd74bdb5
ebrow <abpdx@hotmail.com>
2015-08-21 05:48:21 -0700

    add

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/home.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
+
+      //Variables that you'd like to call on each page go here
+
+      return new ModelAndView(model, layout);
+
+
+    }, new VelocityTemplateEngine());
+  }
+
+  //Algorithm goes here
+}
